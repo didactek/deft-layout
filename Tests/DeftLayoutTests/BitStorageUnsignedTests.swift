7b156fa78f81d540b3d23d1f8cde9296962323fb
Kit Transue <src@kittransue.net>
2020-05-09 12:03:31 -0700

    Add test for unsigned ints for BitStorageCore

---
+++ Tests/DeftLayoutTests/BitStorageUnsignedTests.swift
@@ -0,0 +1,71 @@
+//
+//  BitStorageUnsignedTests.swift
+//  DeftUnitTests
+//
+//  Created by Kit Transue on 2020-05-09.
+//  Copyright © 2020 Kit Transue.
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import XCTest
+
+class BitStorageUnsignedTests: XCTestCase {
+    class UnsignedRange: BitStorageCore {
+        @position(SubByte(ofByte: 1, msb: 7, lsb: 4))
+        var highNibble: UInt8 = 0b1010
+
+        @position(SubByte(ofByte: 1, msb: 3, lsb: 2))
+        var midWord: UInt8 = 0b00
+
+        @position(SubByte(ofByte: 1, msb: 1, lsb: 0))
+        var lastWord: UInt8 = 0b11
+    }
+    var coder = UnsignedRange()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testInit() throws {
+        XCTAssert(coder.storage.bytes.count == 1, "described bits should fit in single byte")
+        XCTAssert(coder.highNibble == 0b1010, "initial value preserved")
+        XCTAssert(coder.midWord == 0b00, "initial value preserved")
+        XCTAssert(coder.lastWord == 0b11, "initial value preserved")
+
+        XCTAssert(coder.storage.bytes[0] == 0b1010_0011, "encoding positions")
+    }
+
+    func testWrite() throws {
+        coder.storage.bytes[0] = 0
+
+        coder.lastWord = 3
+        XCTAssert(coder.storage.bytes[0] == 0b0000_0011, "set low bits")
+        coder.midWord = 3
+        XCTAssert(coder.storage.bytes[0] == 0b0000_1111, "set mid bits")
+        coder.highNibble = 15
+        XCTAssert(coder.storage.bytes[0] == 0b1111_1111, "set high bits")
+
+        coder.highNibble = 0
+        XCTAssert(coder.storage.bytes[0] == 0b0000_1111, "clear high bits")
+        coder.midWord = 0
+        XCTAssert(coder.storage.bytes[0] == 0b0000_0011, "clear mid bits")
+        coder.lastWord = 0
+        XCTAssert(coder.storage.bytes[0] == 0b0000_0000, "clear low bits")
+    }
+
+    func testReadUnderlying() throws {
+        coder.storage.bytes[0] = 0xff
+        XCTAssert(coder.highNibble == 15, "decoding all underlying bits set")
+        XCTAssert(coder.midWord == 3, "decoding all underlying bits set")
+        XCTAssert(coder.lastWord == 3, "decoding all underlying bits set")
+
+        coder.storage.bytes[0] = 0x00
+        XCTAssert(coder.highNibble == 0, "decoding all underlying bits clear")
+        XCTAssert(coder.midWord == 0, "decoding all underlying bits clear")
+        XCTAssert(coder.lastWord == 0, "decoding all underlying bits clear")
+    }
+}
