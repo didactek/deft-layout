c33dce6ebe6f37a7f6a30400da219844090a29d3
Kit Transue <src@kittransue.net>
2020-05-09 12:05:47 -0700

    tidy BitStorageBoolTests: - remove redundant "Bool" from testcase names (it's implied by the scope) - factor out the coder (Object Under Test)

--- Tests/DeftLayoutTests/BitStorageBoolTests.swift
+++ Tests/DeftLayoutTests/BitStorageBoolTests.swift
@@ -20,6 +20,7 @@ class BitStorageBoolTests: XCTestCase {
         @position(SubByte(ofByte: 1, bit: 2))
         var mid = true
     }
+    var coder = BoolAndBit()  // object under test
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -29,8 +30,7 @@ class BitStorageBoolTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testBoolInit() throws {
-        let coder = BoolAndBit()
+    func testInit() throws {
         XCTAssert(coder.storage.bytes.count == 1, "described bits should fit in single byte")
         XCTAssert(coder.msb == true, "initial value preserved")
         XCTAssert(coder.lsb == false, "initial value preserved")
@@ -39,9 +39,7 @@ class BitStorageBoolTests: XCTestCase {
         XCTAssert(coder.storage.bytes[0] == 0b1000_0100, "encoding positions")
     }
 
-    func testBoolWrite() throws {
-        let coder = BoolAndBit()
-
+    func testWrite() throws {
         // FIXME compiler lets me alter a 'let': does this expose an error in mutation sematics?
         coder.msb = false
         XCTAssert(coder.storage.bytes[0] == 0b0000_0100, "clear top bit")
@@ -50,18 +48,16 @@ class BitStorageBoolTests: XCTestCase {
         XCTAssert(coder.storage.bytes[0] == 0b0000_0101, "set bottom bit")
     }
 
-    func testBoolReadUnderlying() throws {
-        let coder = BoolAndBit()
-
+    func testReadUnderlying() throws {
         coder.storage.bytes[0] = 0xff
-        XCTAssert(coder.msb == true, "decoding underlying bit")
-        XCTAssert(coder.mid == true, "decoding underlying bit")
-        XCTAssert(coder.lsb == true, "decoding underlying bit")
+        XCTAssert(coder.msb == true, "decoding all underlying bits set")
+        XCTAssert(coder.mid == true, "decoding all underlying bits set")
+        XCTAssert(coder.lsb == true, "decoding all underlying bits set")
 
         coder.storage.bytes[0] = 0x00
-        XCTAssert(coder.msb == false, "decoding underlying bit")
-        XCTAssert(coder.mid == false, "decoding underlying bit")
-        XCTAssert(coder.lsb == false, "decoding underlying bit")
+        XCTAssert(coder.msb == false, "decoding all underlying bits clear")
+        XCTAssert(coder.mid == false, "decoding all underlying bits clear")
+        XCTAssert(coder.lsb == false, "decoding all underlying bits clear")
     }
 
 }
