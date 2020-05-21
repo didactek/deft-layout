66109a39d20dbbc61020dee067920fb936e1e271
Kit Transue <src@kittransue.net>
2020-05-20 22:27:58 -0700

    Add ByteDescription and WordDescription Use in MCP9808 temperature chip

---
+++ Sources/DeftLayout/WordDescription.swift
@@ -0,0 +1,52 @@
+//
+//  WordDescription.swift
+//  radio
+//
+//  Created by Kit Transue on 2020-05-20.
+//  Copyright © 2020 Kit Transue.
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+/// Encode a 16-bit, big-endian word (or fractions thereof) for SMBus use.
+class WordDescription: BitStorageCore {
+    static var byteWidth: Int = 2
+
+    struct PositionOptions: OptionSet {
+        let rawValue: Int
+
+        static let extendNegativeBit = PositionOptions(rawValue: 1 << 0)
+    }
+
+    @propertyWrapper
+    struct Position<T: BitEmbeddable>: CoderAdapter {
+        var coder: ByteCoder
+
+        var wrappedValue: T {
+            get { decodedValue}
+            set { decodedValue = newValue }
+        }
+
+        init(wrappedValue: T, msb: Int, lsb: Int, _ options: PositionOptions = []) {
+            assert(msb >= 0 && msb < (WordDescription.byteWidth * 8))
+            assert(lsb >= 0 && lsb < (WordDescription.byteWidth * 8))
+            assert(msb >= lsb)
+
+            let (msbDistanceToEnd, msbOffset) = msb.quotientAndRemainder(dividingBy: 8)
+            let (lsbDistanceToEnd, lsbOffset) = lsb.quotientAndRemainder(dividingBy: 8)
+
+            self.coder = try! MultiByteCoder(significantByte: msbDistanceToEnd, msb: msbOffset,
+                                             minorByte: lsbDistanceToEnd, lsb: lsbOffset,
+                                             signed: options.contains(.extendNegativeBit),
+                                             storedIn: AssembledMessage.storageBuildInProgress(),
+                                             littleEndian: false
+            )
+            self.wrappedValue = wrappedValue
+        }
+
+        init(wrappedValue: T, bit: Int) {
+            self.init(wrappedValue: wrappedValue, msb: bit, lsb: bit)
+        }
+    }
+}
